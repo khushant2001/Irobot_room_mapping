8e35eabe3d9dde73472c5aac55c38f4807ec1445
Khushant Khurana <70731991+khushant2001@users.noreply.github.com>
2024-04-07 11:54:34 -0400

    Update test.py

--- khushant/src/controller/controller/test.py
+++ khushant/src/controller/controller/test.py
@@ -11,7 +11,13 @@ import numpy as np
 from sensor_msgs.msg import Imu
 import math
 
-obstacles = []
+obstacles = np.array([
+    [0]
+])
+i = 0
+error = [0]
+integral = [0]
+time_step = .001
 class move(Node):
     def __init__(self):
         super().__init__("Moving")
@@ -22,6 +28,7 @@ class move(Node):
             history=QoSHistoryPolicy.KEEP_LAST,
             depth=5
         )
+        self.initial_position = np.array([[0,0]])
         self.dock_sub = self.create_subscription(DockStatus, "/robot_1/dock_status",self.check_dock, qos_profile=qos_profil)
         self.dock_status = None
         self.haz_sub = self.create_subscription(detect, "/robot_1/hazard_detection", self.hazard_detect,qos_profile = qos_profil)
@@ -29,13 +36,20 @@ class move(Node):
         self.ir_sub = self.create_subscription(ir, "/robot_1/ir_intensity", self.ir_sense, qos_profile = qos_profil)
         self.ir_status = [0,0,0,0,0,0,0]
         #self.imu_sub = self.create_subscription(Imu, "/robot_1/imu", self.imu_sense, qos_profile = qos_profil)
-        self.imu_status = None
+        self.imu_status = np.array([[0,0]])
         self.imu_sub = self.create_subscription(Odometry, "/robot_1/odom", self.imu_sense, qos_profile = qos_profil)
     
     def imu_sense(self, imu_msg:Odometry):
+        global i
         #self.get_logger().info("Getting Odometry")
-        self.imu_status = [math.ceil(imu_msg.pose.pose.position._x), math.ceil(imu_msg.pose.pose.position._y)] 
-        print(self.imu_status)
+        self.imu_status = np.array([
+            [math.floor(5*imu_msg.pose.pose.position._x), math.floor(5*imu_msg.pose.pose.position._y)]]) 
+        if i == 0:
+            self.initial_position[0,0] = self.imu_status[0,0]
+            self.initial_position[0,1] = self.imu_status[0,1]
+            i = i + 1
+        obstacle_detection(self.imu_status - self.initial_position)
+
     def ir_sense(self,ir_msg:ir):
         #self.get_logger().info("Value = ")
         self.ir_status[0] = ir_msg.readings[0].value
@@ -55,26 +69,58 @@ class move(Node):
    
     def move_func(self):
         msg = Twist()
-        print(self.ir_status)
-        if self.dock_status == False:
-            setpoint = 20
-            error = setpoint - self.imu_status[0]
-            msg.angular.z = actuation(error)
-            msg.linear.x = 0.4
+        initial = True
+        tolerance = np.array([
+            [5]
+        ])
+        if initial:
+            setpoint = 70
+            error = setpoint - self.ir_status[0]
+            if (self.ir_status[3]) > 20:
+                msg.linear.x = .01
+                msg.angular.z = -0.7
+            else:
+                msg.angular.z = actuation(error)
+                msg.linear.x = 1.0
+            #print(msg.angular.z)
             self.move.publish(msg)
 
-error = [0]
-integral = [0]
-time_step = .01
-ki = 1
-kp = .6
-kd = 0
+            if (self.initial_position[0,0] - self.imu_status[0,1]) < tolerance:
+                initial = False
+
+ki = 0.05
+kp = .027
+kd = 0.001
 def actuation(e):
     error.append(e)
     integral.append(error[-1]*time_step + integral[-1])
     derivative = (error[-1] - error[-2]) / time_step if len(error) > 1 else 0
     control = kp * error[-1] + ki * integral[-1] + kd * derivative
-    return control
+    #print(-control)
+    return (control)
+
+def obstacle_detection(points):
+    points[0,0] = abs(points[0][0])
+    points[0,1] = abs(points[0][1])
+    #print(points[0,0])
+    #print(points[0,1])
+    global obstacles
+    o_shape = obstacles.shape
+    
+    if points[0,0] > o_shape[0]-1:
+        new_column = np.ones((points[0,0],o_shape[1]))
+        
+        obstacles = np.vstack([obstacles, new_column])
+
+    elif points[0,1] > o_shape[1]-1:  
+        new_row = np.zeros((o_shape[0],1))
+        new_row[0] = 1
+        obstacles = np.hstack([obstacles, new_row])
+    else:
+        print("h")
+        obstacles[points[0,0]][points[0,1]] = 1
+    print("New")
+    print(obstacles)
 
 def quaternion_to_euler(q):
     """
